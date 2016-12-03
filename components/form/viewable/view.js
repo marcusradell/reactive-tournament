2cfe25675fcf4ba282cc1bbab8b157b1e854198a
Marcus Nielsen <marcus.nielsen82@gmail.com>
2016-12-03 17:18:50 +0100

    .

--- components/form/viewable/view.js
+++ components/form/viewable/view.js
@@ -1,7 +1,20 @@
-export default function create({h, children}) {
-  const view = children
-  .map(function onMap(childElms) {
-    return h('div', childElms)
+export default function create({h, childrenStream}) {
+  const view = childrenStream
+  .flatMap(function onFlatMap(childrenArray) {
+    return childrenArray
+    .map(function onMap(childComponent, index) {
+      return {
+        view: childComponent.view,
+        index
+      }
+    })
+  })
+  .map(function onMap() {
+    return function reduce() {
+      return h('div', [
+        
+      ])
+    }
   })
 
   return view
--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,43 @@
 const {resolve} = require('path')
-const webpack = require('webpack')
-const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-const isProduction = process.env.NODE_ENV === 'production' 
+const isProduction = process.env.NODE_ENV === 'production'
 
 const config = {
-    entry: './client/main.js',
-    output: {
-      path: resolve(__dirname, 'build'),
-      pathinfo: !isProduction,
-      filename: 'bundle.[chunkhash].js'
-    },
-    context: __dirname,
-    devtool: isProduction ? 'source-map' : 'eval',
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader'
-        },
-        {
-          test: /\.css$/,
-          use: [
-            {
-              loader: "style-loader"
-            },
-            {
-              loader: "css-loader"
-            }
-          ]
-        }
-      ]
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './static/index.html'
-      }),
-      new UglifyJsPlugin({
-        sourceMap: true
-      })
-      // TODO:
-      // DefinePlugin
-      // UglifyPlugin (Settings)
-      // DedupePlugin
-      // LoaderOptionsPlugin
+  entry: './client/main.js',
+  output: {
+    path: resolve(__dirname, 'build'),
+    pathinfo: !isProduction,
+    filename: 'bundle.[chunkhash].js'
+  },
+  context: __dirname,
+  devtool: isProduction ? 'source-map' : 'eval',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './static/index.html'
+    })
+  ]
+}
 
 module.exports = function onWebpack() {
   return config
